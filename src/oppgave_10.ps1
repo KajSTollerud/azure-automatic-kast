3cefdca65bb004825103f95e7a2cc05426feff22
Kaj Ståle Tollerud <kaj.stale.tollerud@nav.no>
2022-05-11 14:19:15 +0200

    oppgave 10 utført

---
+++ src/oppgave_10.ps1
@@ -0,0 +1,132 @@
+[CmdletBinding()]
+param (
+    # parameter er ikke obligatorisk siden vi har default verdi
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    # når paramater ikke er gitt brukes default verdi
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+$kortstokkJson = $webRequest.Content
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+#foreach ($kort in $kortstokk) {
+#    Write-Output $kort
+#}
+
+# 2. utgave - interessert i 1. karakter i merke - (S)PADE - og verdi
+#foreach ($kort in $kortstokk) {
+#    Write-Output "$($kort.suit[0])+$($kort.value)"
+#}
+
+# 3. utgave - ønsker egentlig hele kortstokken som en streng og den koden som en funksjon (gjenbruk)
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+# videre fra opg 4...
+### Regn ut den samlede poengsummen til kortstokk
+#   Nummererte kort har poeng som angitt på kortet
+#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
+#   Ess (A) teller som 11 poeng
+# 2. utgave - ønsker koden som en funksjon - hvorfor?
+# https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-switch?view=powershell-7.1
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        # Undersøk hva en Switch er
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J','Q','K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+
+$meg = $kortstokk[0..1]
+# Fjern 2 kort fra kortstokken som er gitt til $meg
+$kortstokk = $kortstokk[2..($kortstokk.Count -1)]
+
+$magnus = $kortstokk[0..1]
+# Fjern 2 kort fra kortstokken som er gitt til $magnus
+$kortstokk = $kortstokk[2..($kortstokk.Count -1)]
+
+# ...
+function skrivUtResultat {
+    param (
+        [string]
+        $vinner,        
+        [object[]]
+        $kortStokkMagnus,
+        [object[]]
+        $kortStokkMeg        
+    )
+    Write-Output "Vinner: $vinner"
+    Write-Output "magnus | $(sumPoengKortstokk -kortstokk $kortStokkMagnus) | $(kortstokkTilStreng -kortstokk $kortStokkMagnus)"    
+    Write-Output "meg    | $(sumPoengKortstokk -kortstokk $kortStokkMeg) | $(kortstokkTilStreng -kortstokk $kortStokkMeg)"
+}
+
+# bruker 'blackjack' som et begrep - er 21
+$blackjack = 21
+
+if ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack -and (sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+    skrivUtResultat -vinner 'draw' -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack) {
+    skrivUtResultat -vinner 'meg' -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}        
+elseif ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+# ...
+# meg trekker kort hvis under 17
+while ((sumPoengKortstokk -kortstokk $meg) -lt 17) {
+    $meg += $kortstokk[0]
+    $kortstokk = $kortstokk[1..($kortstokk.Count -1)]
+}
+
+if ((sumPoengKortstokk -kortstokk $meg) -gt $blackjack) {
+    skrivUtResultat -vinner 'Magnus' -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+#magnus trekker hvis mindre en meg
+while ((sumPoengKortstokk -kortstokk $magnus) -le (sumPoengKortstokk -kortstokk $meg)) {
+    $magnus += $kortstokk[0]
+    $kortstokk = $kortstokk[1..($kortstokk.Count -1)]
+}
+
+if ((sumPoengKortstokk -kortstokk $magnus) -gt $blackjack) {
+    skrivUtResultat -vinner 'meg' -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+skrivUtResultat -vinner 'Magnus' -kortStokkMagnus $magnus -kortStokkMeg $meg
